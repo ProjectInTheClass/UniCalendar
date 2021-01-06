3c6092bfaaf0505a8c73ae8fa67e554abda425e4
Kim Kyungmin <qaws2051@gmail.com>
2021-01-06 14:25:14 +0900

    Add HomeViewController
    -HomeStorayboard 업데이트
    -HomeViewController

---
+++ TeamProject/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  TeamProject
+//
+//  Created by KM on 2021/01/06.
+//
+
+import Foundation
+import UIKit
+
+class HomeViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
