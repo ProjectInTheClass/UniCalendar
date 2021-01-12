6a492e290a6e81e7cf8cb53f57b2360d4105d69b
n-y-kim <n-y-kim@users.noreply.github.com>
2021-01-12 16:31:13 +0900

    Update SettingViewController.swift

--- UniCalendar/TeamProject/SettingViewController.swift
+++ UniCalendar/TeamProject/SettingViewController.swift
@@ -29,7 +29,6 @@ class SettingViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        //self.viewDidLoad()
     }
 
 
