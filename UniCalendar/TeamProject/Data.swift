e5de430627ead6fe0683a3bfaa5757e8284a1d65
Kim Kyungmin <qaws2051@gmail.com>
2021-01-18 17:51:03 +0900

    Update Data.swift

--- UniCalendar/TeamProject/Data.swift
+++ UniCalendar/TeamProject/Data.swift
@@ -4,27 +4,53 @@
 //
 //  Created by Nayeon Kim on 2021/01/18.
 //
+//
+
+// UserDefaults: by value
+// Event / Category 함수 따로
+// getX, setX
 
 import Foundation
 
 class API {
     static let shared = API()
     
-   var eventInfo: EventInfo? = nil
+    var eventInfo: EventInfo? = nil
+    var categoryInfo: Category? = nil
+    // var userDatabase = UserDefaults.standard
     
     func initDatabase() {
         // User Default에서 가져오는 코드
-        //self.eventInfo = EventInfo(events: [])
+        // self.eventInfo = EventInfo(events: [])
         
+        /*
+          // set Info
+          eventInfo = UserDefaults.object(forKey: "eventInfo")
+          categoryInfo = UserDefaults.object(forKey: "categoryInfo")
+        */
+
     }
     
     func saveDatabase() {
         // User Default로 저장
+        
+        /*
+           // get Info
+           let encoder = JSONEncoder()
+           var encodedEventInfo = encoder.encode(eventInfo)
+         
+           userDatabase.set(encodedEventInfo, forKey: "eventInfo")
+           userDatabase.set(encodedCategoryInfo
+         
+        */
     }
 }
 
 struct EventInfo: Codable {
     var events:[Event]
+}
+struct CategoryInfo: Codable {
+    var categories: [Category]
 }
 
 struct Event: Codable {
@@ -47,6 +73,7 @@ enum Color: Int, Codable {
 }
 
 struct Category: Codable {
-    var categoryName: [String] = ["📓과제", "📝시험", "👥대외활동"]
+    var categoryName: String
+    // var categoryName: [String] = ["📓과제", "📝시험", "👥대외활동"]
     var categoryColor: Color
 }
