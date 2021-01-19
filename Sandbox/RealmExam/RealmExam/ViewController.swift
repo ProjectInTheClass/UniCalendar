baf7d0d2eaa82533d0c9c2f491a6b5a75f1df6c0
JunKyungKim <wnsrudrose@dongguk.edu>
2021-01-19 14:02:30 +0900

    Realm sandbox

--- Sandbox/RealmExam/RealmExam/ViewController.swift
+++ Sandbox/RealmExam/RealmExam/ViewController.swift
@@ -6,12 +6,40 @@
 //
 
 import UIKit
+import RealmSwift
 
-class ViewController: UIViewController {
+class Cart: Object {
+    @objc dynamic var name = ""
+}
 
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+     
+    @IBOutlet weak var textField: UITextField!
+    @IBAction func button(_ sender: Any) {
+    }
+    @IBOutlet weak var tableView: UITableView!
+    
+    var notificationToken : NotificationToken?
+    var realm: Realm?
+    var items: Results<Cart>?
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return (items?.count)!
+    }
+        
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        cell.textLabel?.text = items![indexPath.row].name
+            
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let config = Realm.Configuration(deleteRealmIfMigrationNeeded: true)
+        Realm.Configuration.defaultConfiguration = config
+        realm = tryㅆ
+        
     }
 
 
