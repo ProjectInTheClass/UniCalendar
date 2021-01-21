551eb3eeb0a4c11ec8aa3227e35b3d0746343b22
JunKyungKim <wnsrudrose@dongguk.edu>
2021-01-21 17:19:02 +0900

    sandbox realm update

---
+++ Sandbox/UniCalendar_RealmTest_kjk/TeamProject/EventAddTableViewController.swift
@@ -0,0 +1,60 @@
+//
+//  EventAddTableViewController.swift
+//  TeamProject
+//
+//  Created by KM on 2021/01/19.
+//
+
+import UIKit
+import RealmSwift
+
+let api = API.shared
+
+class EventAddTableViewController: UITableViewController {
+
+    var dateFormatter:DateFormatter {
+        let df = DateFormatter()
+        df.dateFormat = "yyyy-MM-dd"
+        return df
+    }
+    
+    @IBAction func cancelModal(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func completeModal(_ sender: Any) {
+        // TODO
+        // Add New event to EventList
+        save()
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+//    @IBAction func DdayDatePicker(_ sender: UIDatePicker) {
+//        let datePickerView = sender
+//
+//    }
+    
+    func save() {
+
+        let d = self.dateFormatter.date(from: "2020-01-28")
+        
+        let newEvent = Event(eventName: "알고리즘과제", eventDday: d!, importance: 3, eventIsDone: true)
+
+        let category = Category(categoryName: "과제1", categoryColor: 0)
+        category.eventsInCategory.append(newEvent)
+                
+        
+        try! api.realm.write{
+            api.realm.add([category])
+        }
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        // Do any additional setup after loading the view.
+    }
+    
+}
---
+++ Sandbox/UniCalendar_RealmTest_kjk/TeamProject/HomeViewController.swift
@@ -0,0 +1,117 @@
+//
+//  HomeViewController.swift
+//  TeamProject
+//
+//  Created by KM on 2021/01/07.
+//
+
+import UIKit
+import RealmSwift
+
+//struct EventItem {
+//    var eventName: String
+//    var dDay: String
+//    var importance: Int
+//    var importanceImage: String
+//    var progressPercent: Float
+//}
+//let eventItems: [Event] = [
+//
+//]
+
+
+
+class HomeViewController: UIViewController  {
+    
+    @IBOutlet weak var tableView: UITableView!
+//
+//    private var events: Results<Event>!
+//
+//
+//    // 섹션당 row 수
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        //print(eventList.accessibilityElementCount())
+//        //return eventList.accessibilityElementCount()
+//        return events.count
+//    }
+//
+//
+//    // indexPath 각 (section, row)에 맞는 cell
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//
+//        let dateFormatter = DateFormatter()
+//        dateFormatter.dateFormat = "yyyy-MM-dd"
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "EventCell", for: indexPath) as! EventCell
+//        let event = events[indexPath.row]
+//        cell.eventNameLabel.text = event.eventName
+//        let today = dateFormatter.string(from: Date.init())
+//        let Ddate = dateFormatter.string(from: events[indexPath.row].eventDday)
+//        let startDate = dateFormatter.date(from: today)!
+//        let endDate = dateFormatter.date(from: Ddate)!
+//        let interval = endDate.timeIntervalSince(startDate)
+//        let days = Int(interval / 86400)
+//
+//
+//        cell.dDayLabel.text = "D - " + String(days)
+//        cell.importanceLabel.text = "중요해요"
+//        //cell.importanceImageLabel.text = String(event.events[indexPath.row].)
+//
+//        cell.progressLabel.text = "영차영차"
+////        cell.progressView.setProgress(event.progressPercent, animated: false)
+////        cell.progressPercentLabel.text = "\(event.progressPercent*100)%"
+//        return cell
+//    }
+//
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        guard let destinationController: HomeDetailViewController = segue.destination as? HomeDetailViewController else { return }
+//        guard let cell: EventCell = sender as? EventCell else { return }
+//
+//        destinationController.dDay = cell.dDayLabel.text!
+//        destinationController.eventName = cell.eventNameLabel.text!
+//        destinationController.progressPercent = cell.progressView.progress
+//
+//        // TODO: 디테일 탭으로 소목표 넘기기
+//
+//    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(Realm.Configuration.defaultConfiguration.fileURL)
+//        tableView.dataSource = self
+//        tableView.delegate = self
+//
+//        let myEvent = Event()
+//
+//        let eventName = "dd"
+//        let eventDday = Date.init()
+//        let importance = 3
+//        let eventIsDone = false
+//
+//        myEvent.eventName = eventName
+//        myEvent.eventDday = eventDday
+//        myEvent.importance = importance
+//        myEvent.eventIsDone = eventIsDone
+//
+//        let myEvent2 = Event()
+//
+//        let eventName2 = "aa"
+//        let eventDday2 = Date.init()
+//        let importance2 = 2
+//        let eventIsDone2 = false
+//
+//        myEvent2.eventName = eventName2
+//        myEvent2.eventDday = eventDday2
+//        myEvent2.importance = importance2
+//        myEvent2.eventIsDone = eventIsDone2
+//
+//        try! API.shared.realm.write {
+//            API.shared.realm.add(myEvent)
+//            API.shared.realm.add(myEvent2)
+//        }
+    }
+    
+//    override func viewWillAppear(_ animated: Bool){
+//        events = API.shared.callEvent()
+//    }
+    
+}
