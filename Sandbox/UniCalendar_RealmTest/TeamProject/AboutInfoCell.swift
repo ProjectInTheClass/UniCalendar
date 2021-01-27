35b1cebf6e1c910c8b1101da821b70c90008a591
n-y-kim <n-y-kim@users.noreply.github.com>
2021-01-27 16:12:11 +0900

    update Setting + add Version

---
+++ Sandbox/UniCalendar_RealmTest/TeamProject/AboutInfoCell.swift
@@ -0,0 +1,24 @@
+//
+//  AboutInfoCell.swift
+//  UniCalendar
+//
+//  Created by Nayeon Kim on 2021/01/27.
+//
+
+import UIKit
+
+class AboutInfoCell: UITableViewCell {
+
+    @IBOutlet weak var aboutImage: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
